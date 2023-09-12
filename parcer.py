ad64b10996471906dd8dce9763ffde2733e527d9
Ilya Kerdyashov <kjhk3107@gmail.com>
2023-09-13 00:09:13 +0500

    yeah

--- parcer.py
+++ parcer.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 # URL страницы, которую вы хотите спарсить
 url = 'https://music.yandex.ru/artist/5054100'
@@ -12,12 +13,20 @@ if response.status_code == 200:
     # Используем BeautifulSoup для разбора HTML-кода страницы
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    # Находим все заголовки новостей на странице (предположим, что они находятся в тегах <h2>)
-    headlines = soup.find_all('h2')
+    # Создаем список для хранения данных
+    data = []
 
-    # Выводим заголовки на экран
-    for headline in headlines:
-        print(headline.text)
+    # Ищем все элементы, которые вы хотите спарсить (в данном случае, <h2> и <p>)
+    headlines = soup.find_all(['h2', 'p'])
+
+    # Итерируемся по найденным элементам и добавляем их в список данных
+    for element in headlines:
+        data.append(element.text)
+
+    # Сохраняем данные в JSON файл
+    with open('parsed_data.json', 'w', encoding='utf-8') as json_file:
+        json.dump(data, json_file, ensure_ascii=False, indent=4)
+
+    print('Данные успешно спарсены и сохранены в parsed_data.json')
 else:
     print('Не удалось загрузить страницу')
-
